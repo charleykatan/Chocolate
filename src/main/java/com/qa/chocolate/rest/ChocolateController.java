b720fe67ce6ca0a180017c45e9179f4fd753d723
charleykatan <charleykatan@gmail.com>
2022-01-27 16:25:52 +0000

    added delete method

--- src/main/java/com/qa/chocolate/rest/ChocolateController.java
+++ src/main/java/com/qa/chocolate/rest/ChocolateController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,10 +33,9 @@ public class ChocolateController {
 	//Update - PUT /PATCH
 	
 	//Delete - DELETE
-	@DeleteMapping
-	public void deleteChocolate(Chocolate c) {
-		this.choco.remove(c);
+	@DeleteMapping("/deleteChoco/{id}")
+	public void deleteChocolate(@PathVariable int id) {
+		this.choco.remove(id);
 	}
-	
 	
 }

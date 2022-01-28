2939f39cd0327b72f07cb9d9a4010a63685efd00
charleykatan <charleykatan@gmail.com>
2022-01-28 13:23:05 +0000

    response entity

--- src/main/java/com/qa/chocolate/rest/ChocolateController.java
+++ src/main/java/com/qa/chocolate/rest/ChocolateController.java
@@ -3,10 +3,16 @@ package com.qa.chocolate.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +23,16 @@ public class ChocolateController {
 
 	private List<Chocolate> choco = new ArrayList<>();
 	
+	// Response entity -> allows us to configure the status code of the response
+	
 	//CRUD
 	//Create - POST
+	//Response to return instead of 200: 201 - created
 	@PostMapping("/createChoco")
-	public void createChocolate(@RequestBody Chocolate c) {
+	public ResponseEntity<Chocolate> createChocolate(@RequestBody Chocolate c) {
 		this.choco.add(c);
+		Chocolate newChoc = this.choco.get(this.choco.size() - 1);
+		return new ResponseEntity<Chocolate>(newChoc, HttpStatus.CREATED);
 	}
 	
 	//Read - GET
@@ -30,12 +41,35 @@ public class ChocolateController {
 		return this.choco;
 	}
 	
+	// Read by ID - GET
+	@GetMapping("/getChoco/{id}")
+	public Chocolate getChocoId(@PathVariable int id) {
+		return this.choco.get(id);
+	}
+	
 	//Update - PUT /PATCH
+	@PutMapping("/update/{id}")
+	public Chocolate updateChocolate(@PathVariable int id, @RequestBody Chocolate c) {
+		this.choco.set(id, c);
+		return this.choco.get(id);
+	}
+	
+	@PatchMapping("/updateBrand/{id}")
+	public Chocolate replaceBrand(@PathParam("brand") String brand, @PathVariable int id) {
+		(this.choco.get(id)).setBrand(brand);
+		return this.choco.get(id);
+	}
+	
+	@PatchMapping("/updateName/{id}")
+	public Chocolate replaceName(@RequestBody String name, @PathVariable int id) {
+		(this.choco.get(id)).setName(name);
+		return this.choco.get(id);
+	}
 	
 	//Delete - DELETE
 	@DeleteMapping("/deleteChoco/{id}")
-	public void deleteChocolate(@PathVariable int id) {
-		this.choco.remove(id);
+	public Chocolate deleteChocolate(@PathVariable int id) {
+		return this.choco.remove(id);
 	}
 	
 }
